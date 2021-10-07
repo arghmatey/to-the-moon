73fceccb7850350ee61d754cf8a0afc6c035dbcf
Sarah <40401149+arghmatey@users.noreply.github.com>
2021-10-07 01:58:00 -0700

    installs mongoose

--- server.js
+++ server.js
@@ -1,9 +1,11 @@
-const express = require('express');
-const path = require('path');
-const favicon = require('serve-favicon');
-const logger = require('morgan');
+var express = require('express');
+var path = require('path');
+var favicon = require('serve-favicon');
+var logger = require('morgan');
 
-const app = express();
+require('./config/database');
+
+var app = express();
 
 app.use(logger('dev'));
 app.use(express.json());
@@ -17,7 +19,7 @@ app.get('/*', function(req, res) {
     res.sendFile(path.join(__dirname, 'build', 'index.html'))
 });
 
-const port = process.env.PORT || 3001;
+var port = process.env.PORT || 3001;
 
 app.listen(port, function() {
     console.log(`Express app is running on port ${port}`)
