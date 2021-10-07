c9cd1c168a0dd0f89bab3bfd80eb7e39238be891
Sarah <40401149+arghmatey@users.noreply.github.com>
2021-10-07 01:16:53 -0700

    adds milestone

--- src/milestone_data.js
+++ src/milestone_data.js
@@ -4,6 +4,7 @@ var milestone_data = [
     {'name': 'Troposphere', 'end': 9, blurb: 'Starting at the surface, the Troposphere is the most dense of the Earth\'s atmosphere.'},
     {'name': 'Stratosphere', 'end': 31, blurb: ''},
     {'name': 'Mesosphere', 'end': 53, blurb: ''},
+    {'name': 'Kármán line', 'end': 62, blurb: ''},
     {'name': 'Thermosephere', 'end': 372, blurb: ''},
     {'name': 'Exosphere', 'end': 6200, blurb: ''},
 ]
