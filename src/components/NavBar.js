9cdfce83164de4e679f2253e637a219ad0305c24
Sarah <40401149+arghmatey@users.noreply.github.com>
2021-10-14 18:05:48 -0700

    adds logout function to navbar

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,35 +1,40 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom';
+import userService from '../services/userService';
 
-const NavBar = (props) => {
-    let navContent = props.user ?
-        <nav>
-            <NavLink 
-                to="/" 
-                className="nav-link">Home</NavLink>
-            <NavLink 
-                to="/" 
-                onClick={props.handleLogout} 
-                className="nav-link">Logout</NavLink>
-        </nav>
-        :
-        <nav>
-            <NavLink 
-                to="/" 
-                className="nav-link">Home</NavLink>
-            <NavLink 
-                to="/login" 
-                className="nav-link">Login</NavLink>
-            <NavLink 
-                to="/signup" 
-                className="nav-link">Signup</NavLink>
-        </nav>
-        ;
+const NavBar = ({user, setUser}) => {
+
+    const handleLogout = () => {
+        userService.logout();
+        setUser(null);
+    }
 
     return (
         <header>
             <h1>To the Moon</h1>
-            {navContent}
+            {user ?
+                <nav>
+                    <NavLink 
+                        to="/" 
+                        className="nav-link">Home</NavLink>
+                    <NavLink 
+                        to="/" 
+                        onClick={handleLogout} 
+                        className="nav-link">Logout</NavLink>
+                </nav>
+            :
+                <nav>
+                    <NavLink 
+                        to="/" 
+                        className="nav-link">Home</NavLink>
+                    <NavLink 
+                        to="/login" 
+                        className="nav-link">Login</NavLink>
+                    <NavLink 
+                        to="/signup" 
+                        className="nav-link">Signup</NavLink>
+                </nav>
+            }
         </header>
     );
 };
