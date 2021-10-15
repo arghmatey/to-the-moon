4007e6031870cbfeb2e9e85118a48bf4465586f8
Sarah <40401149+arghmatey@users.noreply.github.com>
2021-10-14 18:12:15 -0700

    creates useEffect to load steps

--- src/App.js
+++ src/App.js
@@ -1,62 +1,56 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import {
   BrowserRouter as Router,
   Route
   } from 'react-router-dom';
 import './App.scss';
 import data from './milestone_data';
+import tokenService from './services/tokenService';
 import userService from './services/userService';
 import stepService from './services/stepService';
 import NavBar from './components/NavBar';
 import StepForm from './components/StepForm';
+import Progress from './components/Progress';
 import Signup from './pages/Signup';
 import Login from './pages/Login';
 
 function App() {
-  // initialize user if there's a token, otherwise - null
-  const [user, setUser] = useState(userService.getUser());
-
+  const [user, setUser] = useState(tokenService.getUserFromToken());
+  const [steps, setSteps] = useState();
   const [goal] = useState(data[data.length -1]);
-  const [userSteps, setUserSteps] = useState(0);
-  const [milestone, setMilestone] = useState(0);
 
   const handleAuth = () => {
     setUser(userService.getUser());
   }
 
-  const handleLogout = () => {
-    userService.logout();
-    setUser(null);
-  }
-
-  // average stride size is 2.1-2.5 feet long & a mile is 5280 feet
+  // average stride is 2.1-2.5 feet long & a mile is 5280 feet
   // for MVP - all mile entries are rounded to two decimal points
-  const convertToMile = steps => {
-    return +(steps * 2.3 / 5280).toFixed(2);
-  }
-
   const convertToSteps = miles => {
     return +(miles * 5280 / 2.3).toFixed(2);
   }
 
-  const checkMilestone = steps => {
-    const convertedMi = convertToMile(steps);
-    if (convertedMi >= data[milestone].end) {
-      setMilestone(milestone + 1)
-    };
-  }
-
   const handleAddSteps = async steps => {
     const updatedUser = await stepService.add(steps);
-    setUserSteps(updatedUser.totalSteps);
+    setSteps(updatedUser.totalSteps.toLocaleString());
   }
+
+  useEffect(() => {
+    if (user) {
+      async function getUserSteps() {
+        const user = await userService.getUser();
+        setSteps(user.totalSteps.toLocaleString());
+      };
+
+      getUserSteps();
+    }
+  }, [user])
 
   return (
     <Router>
       <div className="app">
           <NavBar
             user={user}
-            handleLogout={handleLogout}
+            setUser={setUser}
           />
 
         <main>
@@ -69,27 +63,15 @@ function App() {
               <section>
                 <h2>The {goal.name} is {goal.end.toLocaleString()} miles away.</h2>
                 <h3>That's only {convertToSteps(goal.end).toLocaleString()} steps!</h3>
-
-                <StepForm 
-                  handleAddSteps={handleAddSteps}
-                />
-
-                <div>You're {userSteps.toLocaleString()} steps closer to the moon!</div>
               </section>
 
-              <section className="milestone-container">
-                <h3>Current milestone: {data[milestone].name}</h3>
-              </section>
-              
-              {user ?
-                <section className="milestone-container">
-                  <div>You're {user.totalSteps.toLocaleString()} steps closer to the moon!</div>
-                </section>
-              :
-                <section className="milestone-container">
-                  <div>Signup to track your progress.</div>
-                </section>
-              }
+              <StepForm 
+                handleAddSteps={handleAddSteps}
+              />
+              <Progress
+                user={user}
+                steps={steps}
+              />
             </Route>
 
             <Route exact path="/signup">
@@ -97,6 +79,7 @@ function App() {
                 handleAuth={handleAuth}
               />
             </Route>
+
             <Route exact path="/login">
               <Login
                 handleAuth={handleAuth}
